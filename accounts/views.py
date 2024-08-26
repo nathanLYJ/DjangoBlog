7a59fa5ed05cfbd296c41b1865309cea7f270c47
nathanLYJ <nathanlyj@gmail.com>
2024-08-27 00:45:44 +0900

    accounts.views.py 회원가입,로그인,로그아옷, 프로필 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+signup = CreateView.as_view(
+    form_class=UserCreationForm,
+    template_name="accounts/form.html",
+    # success_url = settings.LOGIN_URL,
+    success_url="/accounts/login/",
+)
+
+login = LoginView.as_view(
+    template_name="accounts/form.html",
+)
+logout = LogoutView.as_view()
+
+
+@login_required
+def profile(request):
+    return render(request, "accounts/profile.html")
