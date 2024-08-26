b8954786966a6d2608ff824827fb29bc74f26043
nathanLYJ <nathanlyj@gmail.com>
2024-08-27 00:39:11 +0900

    config.urls main&blog&accounts 연동

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", MainPageView.as_view(), name="main"),
     path("blog/", include("blog.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
