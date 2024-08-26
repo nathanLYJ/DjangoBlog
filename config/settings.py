9e5565b976750f6f1d2e2646b836db34aab37aba
nathanLYJ <nathanlyj@gmail.com>
2024-08-27 00:39:50 +0900

    settings  account&blog추가

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog",
     "accounts",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
