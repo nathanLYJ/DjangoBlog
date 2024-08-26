146bc72587b2ec015a4e0228ed2386eeac07e355
nathanLYJ <nathanlyj@gmail.com>
2024-08-26 09:33:18 +0900

    💄 settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!jg^^0u1e62-w*9azc7vqg#cgqo45!-%)4p1+i)amoe!*d7@7i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "asia/seoul"
 
 USE_I18N = True
 
