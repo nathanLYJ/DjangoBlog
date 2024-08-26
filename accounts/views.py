dee0121a9f3df2af54f3f765f6070c15ffdf77c1
nathanLYJ <nathanlyj@gmail.com>
2024-08-27 01:41:55 +0900

    💄 views.py ruls 수정

--- accounts/views.py
+++ accounts/views.py
@@ -6,13 +6,14 @@ from django.views.generic import CreateView
 
 signup = CreateView.as_view(
     form_class=UserCreationForm,
-    template_name="accounts/form.html",
+    template_name="accounts/user_signup.html",
     # success_url = settings.LOGIN_URL,
-    success_url="/accounts/login/",
+    success_url="blog:post_list",
 )
 
 login = LoginView.as_view(
-    template_name="accounts/form.html",
+    template_name="accounts/user_login.html",
+    success_url="blog:post_list",
 )
 logout = LogoutView.as_view()
 
