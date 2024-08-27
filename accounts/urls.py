d537850c0e712b5c014579edcae8471344be75ea
nathanLYJ <nathanlyj@gmail.com>
2024-08-27 12:07:19 +0900

    로그인 사이업 관련 코드 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from .views import UserSignupView, LoginView
+
+app_name = "accounts"
 
 urlpatterns = [
-    path("signup/", views.signup, name="signup"),
-    path("login/", views.login, name="login"),
+    path("signup/", UserSignupView.as_view(), name="user_signup"),
+    path(
+        "login/",
+        LoginView.as_view(template_name="accounts/user_login.html"),
+        name="user_login",
+    ),
     path("logout/", views.logout, name="logout"),
     path("profile/", views.profile, name="profile"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,39 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 from django.views.generic import CreateView
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
 
-signup = CreateView.as_view(
-    form_class=UserCreationForm,
-    template_name="accounts/user_signup.html",
-    # success_url = settings.LOGIN_URL,
-    success_url="blog:post_list",
-)
 
-login = LoginView.as_view(
-    template_name="accounts/user_login.html",
-    success_url="blog:post_list",
-)
+class UserSignupView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "accounts/user_signup.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+
+        messages.success(self.request, "환영합니다! 회원가입이 완료되었습니다.")
+        return redirect(self.success_url)
+
+    success_url = reverse_lazy("blog:post_list")
+
+    def form_invalid(self, form):
+        messages.error(self.request, "회원가입에 실패했습니다. 다시 시도해주세요.")
+        return super().form_invalid(form)
+
+
+class CustomLoginView(LoginView):
+    template_name = "accounts/user_login.html"
+
+    def form_invalid(self, form):
+        messages.error(self.request, "로그인에 실패했습니다. 다시 시도해주세요.")
+        return super().form_invalid(form)
+
+
 logout = LogoutView.as_view()
 
 
