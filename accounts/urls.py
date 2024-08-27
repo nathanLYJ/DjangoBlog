7927b102f836be59ae23464dcc39081e24fad623
nathanLYJ <nathanlyj@gmail.com>
2024-08-27 12:45:42 +0900

    💄 로그인 urls 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from . import views
-from .views import UserSignupView, LoginView
+from .views import UserSignupView, CustomLoginView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("signup/", UserSignupView.as_view(), name="user_signup"),
-    path(
-        "login/",
-        LoginView.as_view(template_name="accounts/user_login.html"),
-        name="user_login",
-    ),
+    path("login/", CustomLoginView.as_view(), name="user_login"),
     path("logout/", views.logout, name="logout"),
     path("profile/", views.profile, name="profile"),
 ]
