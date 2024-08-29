b9c9ccbed15ee92e28c75f6a04d8f7229bf2cd3b
nathanLYJ <nathanlyj@gmail.com>
2024-08-29 12:50:37 +0900

    feat: Add UserProfileAdmin to manage user profiles in the admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from accounts.models import UserProfile
+
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "nickname", "introduction")
+    search_fields = ("user__username", "nickname")
