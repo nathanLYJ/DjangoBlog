dab052fa71bf77ef096d0d7a1fa11bfa20224a92
nathanLYJ <nathanlyj@gmail.com>
2024-08-29 14:01:22 +0900

    feat: Refactor admin.py to remove UserProfileAdmin and update imports

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, Category, Tag, Post, Comment
-
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "nickname", "introduction")
-    search_fields = ("user__username", "nickname")
+from .models import Category, Post
 
 
 @admin.register(Post)
