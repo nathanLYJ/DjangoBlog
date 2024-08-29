f7977f3ceca34ba38d5bc1ed4e7b239ac5d84aba
nathanLYJ <nathanlyj@gmail.com>
2024-08-29 12:50:15 +0900

    feat: Add phone_number field to UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    introduction = models.TextField(blank=True)
+    image = models.ImageField(upload_to="profile_images", blank=True)
+    nickname = models.CharField(max_length=50, unique=True)
+    address = models.CharField(max_length=255, blank=True)
+    birth_date = models.DateField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True)  # 이 필드를 추가
+
+    def __str__(self):
+        return self.nickname or self.user.username
