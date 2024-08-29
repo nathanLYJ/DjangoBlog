b5c1a3c3135b79a8ccbc9da6a92cb03f98321124
nathanLYJ <nathanlyj@gmail.com>
2024-08-29 15:29:13 +0900

    feat: Add static file serving for media files in development mode

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import MainPageView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,3 +10,6 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
     path("accounts/", include("accounts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
