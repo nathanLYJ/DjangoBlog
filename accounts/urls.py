398a2e5d2bb83cf44c0a8877ea412284405e3811
nathanLYJ <nathanlyj@gmail.com>
2024-09-02 09:40:24 +0900

    Refactor URL patterns for password change functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,14 +12,6 @@ urlpatterns = [
     path("logout/", views.logout, name="logout"),
     path("profile/", views.ProfileView.as_view(), name="profile"),
     path("profile/update/", views.ProfileUpdateView.as_view(), name="profile_update"),
-    path(
-        "password_change/", CustomPasswordChangeView.as_view(), name="password_change"
-    ),
-    path(
-        "password_change/done/",
-        PasswordChangeDoneView.as_view(
-            template_name="accounts/password_change_done.html"
-        ),
-        name="password_change_done",
-    ),
+    path("password_change/", CustomPasswordChangeView.as_view(), name="password_change"),
+    path("password_change/done/",PasswordChangeDoneView.as_view(template_name="accounts/password_change_done.html"), name="password_change_done"),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -13,14 +13,8 @@ urlpatterns = [
     path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_edit"),
     path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
     path("posts/<int:pk>/comment/", views.add_comment, name="add_comment"),
-    path(
-        "posts/<int:post_pk>/comment/<int:comment_pk>/reply/",
-        views.add_reply,
-        name="add_reply",
-    ),
-    path(
-        "category/<int:pk>/", views.CategoryDetailView.as_view(), name="category_detail"
-    ),
+    path("posts/<int:post_pk>/comment/<int:comment_pk>/reply/",views.add_reply,name="add_reply",),
+    path("category/<int:pk>/", views.CategoryDetailView.as_view(), name="category_detail"),
     path("search/", views.PostSearchView.as_view(), name="post_search"),
     path("comment/<int:pk>/delete/", views.blog_comment_delete, name="delete_comment"),
 ]
