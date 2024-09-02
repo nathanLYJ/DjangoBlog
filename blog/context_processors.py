d6825c520f0e32553d5110254b66d7f058e13aa5
nathanLYJ <nathanlyj@gmail.com>
2024-09-02 12:55:32 +0900

    포스트 캐시 관리

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,30 +1,25 @@
 from django.core.cache import cache
-from .models import Category
+from .models import Category, Post
 
 
 def categories(request):
     categories = cache.get("all_categories")
     if not categories:
         categories = list(Category.objects.all())
-        cache.set("all_categories", categories, 3600)  # 1시간 동안 캐시
+        cache.set("all_categories", categories, 3600) 
     return {"categories": categories}
 
-from .models import Post
 
 def get_post_file(post_id):
-    # 캐시에서 파일 정보 가져오기
     cache_key = f"post_{post_id}_file_upload"
     file_info = cache.get(cache_key)
     
     if not file_info:
-        # 캐시에 파일 정보가 없으면 데이터베이스에서 쿼리
         try:
             post = Post.objects.get(pk=post_id)
-            file_info = post.file_upload  # 파일 정보 가져오기
+            file_info = post.file_upload  
         except Post.DoesNotExist:
-            file_info = None
-        
-        # 캐시에 파일 정보 저장 (1시간 동안 캐시)
-        cache.set(cache_key, file_info, 3600)  # 1시간 동안 캐시
+            file_info = None    
+        cache.set(cache_key, file_info, 3600)  
     
     return file_info
